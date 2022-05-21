f540ec6e425879feabc836fc0402a2e0aa62197e
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2022-05-21 19:50:32 +0700

    21 May
    Modify animation of index.html, add js

---
+++ js/tveffect.js
@@ -0,0 +1,6 @@
+function zoomOut() {
+  background = document.getElementById("bg");
+  background.className = "bg-small";
+  // ingredient_panel = document.getElementsByClassName("ingredient-container");
+  // ingredient_panel.style.display = "grid";
+}
