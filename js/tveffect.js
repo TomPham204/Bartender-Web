6eb65614a87cc7b14b4e82417a22e7913c90d6e2
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2022-05-21 21:17:43 +0700

    21 May
    update index.html animation

--- js/tveffect.js
+++ js/tveffect.js
@@ -1,6 +1,6 @@
 function zoomOut() {
   background = document.getElementById("bg");
   background.className = "bg-small";
-  // ingredient_panel = document.getElementsByClassName("ingredient-container");
-  // ingredient_panel.style.display = "grid";
+  ingredient_panel = document.getElementById("i-panel");
+  ingredient_panel.className = "ingredient-container visible";
 }
