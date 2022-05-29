a2f4b914d8796f6ebcfa0e57874a848aa5819afe
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2022-05-29 21:12:49 +0700

    Update click function for index.html

--- js/tveffect.js
+++ js/tveffect.js
@@ -3,4 +3,15 @@ function zoomOut() {
   background.className = "bg-small";
   ingredient_panel = document.getElementById("i-panel");
   ingredient_panel.className = "ingredient-container visible";
+  button = document.getElementById("btn");
+  btn.style.visibility = "hidden";
+}
+
+function clicked(element) {
+  element.style.visibility = "inherit";
+  element.style.border = "5px solid rgba(255, 255, 255, 0)";
+}
+
+function resetState(element) {
+  element.style.border = "none";
 }
