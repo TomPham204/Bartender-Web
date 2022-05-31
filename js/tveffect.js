56b3163cc52fb9f6ae7274efbb057f04106e924c
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2022-05-31 16:35:04 +0700

    Big update May 31st
    -Add function to reset chosen state of ingredients
    -Add function to detect if chosen 3 ingredients? Show a drink : {console.log not enough ingredient and call reset state}
    -Add button to call the actions above
    -If button clicked when a drink is finished, delete the drink from the screen

--- js/tveffect.js
+++ js/tveffect.js
@@ -1,3 +1,6 @@
+var count = 0; //use var for bigger scope. Let is for within a local function only
+var isCompleted = false;
+
 function zoomOut() {
   background = document.getElementById("bg");
   background.className = "bg-small";
@@ -10,8 +13,40 @@ function zoomOut() {
 function clicked(element) {
   element.style.visibility = "inherit";
   element.style.border = "5px solid rgba(255, 255, 255, 0)";
+  count += 1;
+  console.log(element.id);
+  console.log(count);
 }
 
-function resetState(element) {
-  element.style.border = "none";
+function resetState() {
+  elements = document.getElementsByClassName("ingredient-img");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.border = "none";
+  }
+}
+
+function showBottle() {
+  item = document.getElementById("bottle");
+  item.style.display = "block";
+}
+
+function hideBottle() {
+  item = document.getElementById("bottle");
+  item.style.display = "none";
+}
+
+function verify() {
+  if (count === 3) {
+    if (isCompleted == true) {
+      hideBottle();
+      resetState();
+      return;
+    }
+    showBottle();
+    isCompleted = true;
+  } else {
+    console.log(count + " not enough ingredients");
+    resetState();
+    count = 0;
+  }
 }
