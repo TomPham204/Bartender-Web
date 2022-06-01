81a14028514bca1ff1324ce28853b623ca76178e
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2022-06-01 09:53:28 +0700

    Big update #2
    Updated multi functions in JS file.

--- js/tveffect.js
+++ js/tveffect.js
@@ -1,5 +1,8 @@
-var count = 0; //use var for bigger scope. Let is for within a local function only
+var ingredientCount = 0; //use var for bigger scope. Let is for within a local function only
+var idCount = 0; //use var for bigger scope
 var isCompleted = false;
+var idChosen = [];
+var base = ["coffee", "juice", "tea"];
 
 function zoomOut() {
   background = document.getElementById("bg");
@@ -13,20 +16,27 @@ function zoomOut() {
 function clicked(element) {
   element.style.visibility = "inherit";
   element.style.border = "5px solid rgba(255, 255, 255, 0)";
-  count += 1;
-  console.log(element.id);
-  console.log(count);
+  ingredientCount += 1;
+  idChosen.push(element.id);
 }
 
 function resetState() {
+  console.log("resetted");
   elements = document.getElementsByClassName("ingredient-img");
   for (var i = 0; i < elements.length; i++) {
     elements[i].style.border = "none";
   }
+  hideBottle();
+  ingredientCount = 0;
+  isCompleted = false;
+  idChosen = [];
+  idCount = 0;
 }
 
 function showBottle() {
   item = document.getElementById("bottle");
+  item.style.display = "block";
+  item = document.getElementById("bottle-bg");
   item.style.display = "block";
 }
 
@@ -36,17 +46,33 @@ function hideBottle() {
 }
 
 function verify() {
-  if (count === 3) {
+  // check if already show bottle
+  if (ingredientCount === 3) {
     if (isCompleted == true) {
-      hideBottle();
       resetState();
       return;
     }
-    showBottle();
-    isCompleted = true;
+    // check if chosen 1 main ingredient (base) and 2 additional mixes
+    for (var i = 0; i < idChosen.length; i++) {
+      for (var j = 0; j < base.length; j++) {
+        if (idChosen[i] === base[j]) {
+          idCount += 1;
+        }
+      }
+    }
+    i = 0;
+    j = 0;
+    if (idCount == 1) {
+      showBottle();
+      isCompleted = true;
+    } else {
+      console.log("You should choose only 1 main and 2 other mixes");
+      resetState();
+    }
   } else {
-    console.log(count + " not enough ingredients");
+    // check if choose more or less than 3 ingredients
+    console.log(ingredientCount + " is not the right number of ingredients");
     resetState();
-    count = 0;
+    return;
   }
 }
