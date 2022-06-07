e2aff64667782ab1e39921d07f8c01c4420e5b13
Lê Thanh Phương Nam <75212572+NamBobby@users.noreply.github.com>
2022-06-07 23:19:16 +0700

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -2,12 +2,12 @@ create database user_db;
 use user_db;
 
 CREATE TABLE user_form (
-  id int NOT NULL,
+  id int(100) NOT NULL auto_increment,
   name varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
-  user_type varchar(255) NOT NULL DEFAULT 'user',
   password varchar(255) NOT NULL,
+  user_type varchar(255) NOT NULL DEFAULT 'user',
   PRIMARY KEY (id)
 );
 
-INSERT INTO user_db.`user_form` (id, name, email, user_type, password) VALUES ('1', 'admin', 'admin@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO user_db.`user_form` (name, email, password, user_type) VALUES ('admin', 'admin@gmail.com', 'admin', 'admin');
